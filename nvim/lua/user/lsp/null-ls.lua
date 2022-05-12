e2b0988fb92afa15d2a6eeac710710484d9e000d
Lucas Liu <lucas.liu@novatti.com>
2022-05-12 18:03:39 +1000

    lsp

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -15,6 +15,7 @@ null_ls.setup({
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
     formatting.rustfmt,
+    formatting.terraform_fmt,
     -- diagnostics.flake8
 	},
 })
