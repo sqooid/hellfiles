d3d27b08eff4f8fac5aab7dbac7a34f157cf4d76
lucas <lucasliu7@hotmail.com>
2022-05-13 12:31:12 +1000

    added servers

--- nvim/lua/user/lsp/configs.lua
+++ nvim/lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "pyright" }
+local servers = { "jsonls", "sumneko_lua", "pyright", "clangd", "rust_analyzer" }
 
 lsp_installer.setup {
 	ensure_installed = servers
