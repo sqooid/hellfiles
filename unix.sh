992d3b1bad8b9b8a0d95fcbb4123f8d0c5dfd86d
Lucas Liu <sqooiddev@gmail.com>
2022-10-04 19:00:15 +1100

    added jetbrains font

--- unix.sh
+++ unix.sh
@@ -8,3 +8,9 @@ $DIR/shared-init/unix.sh
 
 # Configs
 find $DIR_ABS/config -mindepth 1 -maxdepth 1 | xargs ln -v -s -f -t $HOME/.config
+
+# Fonts
+mkdir -p /tmp && cd /tmp
+curl -sLOJ 'https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip' 
+unzip JetBrainsMono.zip '*NL*Mono.ttf' -d $HOME/.fonts
+rm JetBrainsMono.zip
