2e37eb23acfdf6d79b5dbc85448846396553ac6a
Lucas Liu <sqooiddev@gmail.com>
2022-10-04 12:11:26 +1100

    changed structure

--- linux.sh
+++ linux.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 DIR=$(dirname $0)
+DIR_ABS=$(cd $DIR; pwd)
 
 # Shared
 $DIR/shared-init/unix.sh
 
-# Nvim
-ln -s -f $PWD/nvim $HOME/.config/nvim
-sudo ln -s -f $PWD/scripts/* /usr/local/bin/
+# Configs
+find $DIR_ABS/config -mindepth 1 -maxdepth 1 | xargs ln -v -s -f -t $HOME/.config
