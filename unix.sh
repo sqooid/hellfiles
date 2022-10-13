92e70bcf24ca1c1ee8a4c9843a690ff30343e6ca
Lucas Liu <lucas.liu@novatti.com>
2022-10-14 10:02:16 +1100

    fixed font directory for mac

--- unix.sh
+++ unix.sh
@@ -3,14 +3,16 @@
 DIR=$(dirname $0)
 DIR_ABS=$(cd $DIR; pwd)
 
-# Shared
-$DIR/shared-init/unix.sh
-
 # Configs
-find $DIR_ABS/config -mindepth 1 -maxdepth 1 | xargs ln -v -s -f -t $HOME/.config
+find $DIR_ABS/config -mindepth 1 -maxdepth 1 |  xargs -I {} ln -v -s -f {} $HOME/.config
 
 # Fonts
 mkdir -p /tmp && cd /tmp
 curl -sLOJ 'https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip' 
+if [[ $OSTYPE == 'darwin'* ]]; then
+  FONT_DIR=$HOME/Library/Fonts
+else 
+  FONT_DIR=$HOME/.fonts
+fi
 unzip JetBrainsMono.zip '*NL*Mono.ttf' -d $HOME/.fonts
 rm JetBrainsMono.zip
